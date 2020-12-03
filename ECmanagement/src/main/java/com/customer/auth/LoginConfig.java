1c2b6071767ca02b8f5bd24eddcd80fd6cffbef1
daijiro_kikuchi <daijiro.kikuchi@comsize.com>
2020-12-03 09:50:26 +0900

    ログイン機能のConfig設定

--- ECmanagement/src/main/java/com/customer/auth/LoginConfig.java
+++ ECmanagement/src/main/java/com/customer/auth/LoginConfig.java
@@ -1,5 +1,11 @@
 package com.customer.auth;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+//パスを受けとりログイン画面に遷移
 public class LoginConfig {
 
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+	}
 }
--- ECmanagement/src/main/java/com/customer/auth/WebSecurityConfig.java
+++ ECmanagement/src/main/java/com/customer/auth/WebSecurityConfig.java
@@ -2,16 +2,22 @@ package com.customer.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.customer.service.UserService;
 
 //ログイン機能を設定するクラス
-public class WebSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	  
 
 	  @Autowired
@@ -37,7 +43,7 @@ public class WebSecurityConfig {
 	    web.ignoring().antMatchers("/login-create-save/**");
 	  }
 
-	  //@Override
+	  @Override
 	  protected void configure(HttpSecurity http) throws Exception {
 	    http.authorizeRequests().mvcMatchers("/login").permitAll() // ログイン画面は誰にでも入れるよ
 	        .mvcMatchers("/admin/**").hasRole("ADMIN") // adminしか閲覧できない、が実装は後回し
