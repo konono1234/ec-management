8efdbcb0f6640fd10cd6bf9362268619b3c6bec5
daijiro_kikuchi <daijiro.kikuchi@comsize.com>
2020-12-03 16:47:43 +0900

    SQL Server 接続設定＆ログイン画面用コントローラー完成(仮)

--- ECmanagement/src/main/java/com/customer/auth/AuthController.java
+++ ECmanagement/src/main/java/com/customer/auth/AuthController.java
@@ -1,5 +1,37 @@
 package com.customer.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class AuthController {
 	
+	final static Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+	/**
+	 * ログイン画面を表示する
+	 * 
+	 * @return String
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "auth/login";
+	}
+
+	/**
+	 * ログインエラー画面を表示する
+	 * 
+	 * @param model
+	 * @return String
+	 */
+	@RequestMapping(value = "/login-error", method = RequestMethod.GET)
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "auth/login";
+	}
+
 }
