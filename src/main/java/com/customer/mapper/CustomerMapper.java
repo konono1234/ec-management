52753ca01d907d9818aebf215ae3540d465e0cf4
konoue <yuki.konoue@comsize.com>
2020-12-04 11:52:19 +0900

    xml連携成功

--- src/main/java/com/customer/mapper/CustomerMapper.java
+++ src/main/java/com/customer/mapper/CustomerMapper.java
@@ -2,16 +2,16 @@ package com.customer.mapper;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.springframework.transaction.annotation.Transactional;
 import com.customer.bean.CustomerBean;
 
 /*
  * DBにアクセスするマッパークラスです
  */
-
+@Transactional
 @Mapper
 public interface CustomerMapper {
 
-  @Select("select * from customer")
+  // @Select("select * from customer")
   public List<CustomerBean> findAll();
 }
